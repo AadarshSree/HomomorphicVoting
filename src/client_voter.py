03ffb8407b988a90b26f1b943fbcee434b38ed2d
Aadarsh Sreekumar <aadarshs@Aadarshs-MacBook-Pro.local>
2024-04-01 12:33:56 -0400

    init

---
+++ src/client_voter.py
@@ -0,0 +1,88 @@
+import tenseal as ts
+import sqlite3
+import os
+import random
+
+username = ""
+vote_list = [random.randint(1000,9999),0,0,0]
+
+candidatesDict = {1: "Yeti", 2: "Big Mac", 3: "BuffaloJ91"}
+
+DB_TABLE_NAME = "testElection"
+
+def insert_vote_into_db():
+
+    connection = sqlite3.connect("./../db/voteDB.db")
+    cursor = connection.cursor()
+
+    try:
+        with open('./../keys/publicKey_bfv.hex', 'r') as f:
+            public_context = ts.context_from(bytes.fromhex(f.read()))
+        encryptedVoteList = ts.bfv_vector(public_context, vote_list)
+
+        vote_hex = encryptedVoteList.serialize().hex() 
+        # print(username)
+        cursor.execute(f"INSERT into {DB_TABLE_NAME} (username,secretVote) values (?,?)", (username, vote_hex))
+        connection.commit()
+    except Exception as e:
+        print(e)
+        connection.rollback()
+
+    connection.close()
+    return
+
+def print_vote_selection_menu():
+    os.system('clear')
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print(f"Hi {username}, Please select a candidate to vote for:")
+    print("1. "+candidatesDict[1])
+    print("2. "+candidatesDict[2])
+    print("3. "+candidatesDict[3])
+    print("4. Exit")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    selection = input("Enter your selection: ")
+
+    if selection not in ['1','2','3','4']:
+        print("[*] Invalid selection, please try again...")
+        os.system('sleep 1')
+        return print_vote_selection_menu()
+    return selection
+
+def print_voter_login_menu():
+    os.system('clear')
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print("Welcome to Homomorphic e-Voting !")
+    t_username = input("Username: ")
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    print("Hello " + t_username + ", Welcome!")
+    os.system('sleep 1')
+    
+    return t_username
+
+def vote_handler():
+    global username, vote_list
+    username = str(print_voter_login_menu())
+    vote_id = print_vote_selection_menu()
+
+    if vote_id == '4':
+        print("[*] Exiting...")
+        exit(0)
+    
+    print("[*] You have selected candidate#" + vote_id + " - " + candidatesDict[int(vote_id)])
+    vote_list[int(vote_id)] = 1
+    print(vote_list)
+
+    try:
+        insert_vote_into_db()
+    except Exception as e:
+        print("[*] Error inserting vote into db: " + str(e))
+        exit(1)
+
+    print("[*] Your Vote has been recorded!")
+    print("+---------------------------------------------------------------+")    
+
+if __name__ == "__main__":
+    print("[*] Hello World!")
+
+
+    vote_handler()
---
+++ src/server_tally.py
@@ -0,0 +1,65 @@
+import tenseal as ts
+import sqlite3
+import os
+
+candidatesDict = {1: "Yeti", 2: "Big Mac", 3: "BuffaloJ91"}
+DB_TABLE_NAME = "testElection"
+
+
+def count_all_votes():
+
+    # connection = sqlite3.connect("/Users/aadarshs/Documents/QueensU/WINTER23/Cryptography/Project/eVote/db/voteDB.db")
+    connection = sqlite3.connect("./../db/voteDB.db")
+    cursor = connection.cursor()
+
+    cursor.execute(f"SELECT ID, secretVote FROM {DB_TABLE_NAME}")
+    rows = cursor.fetchall()
+
+    # for r in rows:
+    #     print(r[0])
+
+    #read the public KEY in
+    with open('./../keys/publicKey_bfv.hex', 'r') as f:
+        publicContext = ts.context_from(bytes.fromhex(f.read()))
+
+    # print(decrypt_result(rows[1][1]))
+    #now go line by line and print secretVote
+    sum = [0,0,0,0]
+    for r in rows:
+        encVector = ts.bfv_vector_from(publicContext, bytes.fromhex(r[1]))
+        sum = sum + encVector
+
+
+    counted_result = decrypt_result(sum.serialize().hex())[1:]
+    max_index = counted_result.index(max(counted_result))+1
+    print("[*] The Vote Tally: ",counted_result)
+    # print(f"[*] The Winner is: {candidatesDict[max_index]}")
+
+    #need to implement tie logic
+
+    if counted_result.count(max(counted_result)) == 1:
+        print(f"[*] The Winner is: {candidatesDict[max_index]}")
+
+    else:
+        print("[*] There is a tie!")
+        tie_candidates = [candidatesDict[i] for i, count in enumerate(counted_result, start=1) if count == max(counted_result)]
+        print("[*] The Candidates who have tied: ", tie_candidates)
+
+        
+    connection.close()
+
+def decrypt_result(result_hex):
+    #read the secret KEY in
+    with open('./../keys/privateKey_bfv.hex', 'r') as f:
+        privateContext = ts.context_from(bytes.fromhex(f.read()))
+
+    #decrypt the result
+    result = ts.bfv_vector_from(privateContext, bytes.fromhex(result_hex))
+    return result.decrypt(privateContext.secret_key())
+
+if __name__ == "__main__":
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+    print("[*] Welcome to Homomorphic Vote Counting!\n")
+    count_all_votes()
+    print("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    # print(os.path.abspath('.'))
